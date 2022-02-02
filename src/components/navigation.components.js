48f3f99f479e9eecdbb41c3758d28c6c5a2da6d4
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-02-02 13:31:49 +0300

    change tabs code and hovet effect

--- src/components/navigation.components.js
+++ src/components/navigation.components.js
@@ -9,25 +9,6 @@ export class NavigationComponents extends Component {
   registerTabs(tabs) {
     this.tabs = tabs;
   }
-
-  // Получаем элемент навигации из массива this.tabs
-  find(dataName) {
-    const activeTab = this.tabs.find(({ name }) => {
-      return name === dataName;
-    });
-    // console.log(activeTab);
-
-    this.tabs.forEach((tab) => {
-      if (tab === activeTab) {
-        activeTab.component.show();
-      } else {
-        tab.component.hide();
-      }
-      // console.log(tab);
-      // console.log(activeTab)
-    });
-  }
-
   init() {
     this.$el.addEventListener('click', tabClickHandler.bind(this));
   }
@@ -39,15 +20,26 @@ function tabClickHandler(e) {
   // получаем массив табов
   let tabsArray = Array.from(this.$el.querySelectorAll('.tabs__item'));
 
-  //Перебираем табы, удаляем у всех 'active' и навешиваем 'active' - 'e.target'
+  //Перебираем табы, удаляем у всех 'active' и навешиваем 'active' - 'e.target' для эффекта переключения
   if (e.target.classList.contains('tabs__item')) {
     tabsArray.forEach((tab) => tab.classList.remove('active'));
   }
   e.target.classList.add('active');
 
-  // С помощью данного метода получаем необходимый элемент из массива this.tabs, содержащий соответствующий табу компонент
+  // Получаем элемент навигации из массива this.tabs
+  const activeTab = this.tabs.find(({ name }) => {
+    return name === e.target.dataset.name;
+  });
+  // console.log(activeTab);
 
-  this.find(e.target.dataset.name);
-
+  this.tabs.forEach((tab) => {
+    if (tab === activeTab) {
+      activeTab.component.show();
+    } else {
+      tab.component.hide();
+    }
+    // console.log(tab);
+    // console.log(activeTab)
+  });
   //   console.log(this);
 }
